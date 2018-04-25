f0a376951c429b8f8ac88b5ca9205e1118c5d5d8
1amvivek <viveklakshmanan@live.com>
2018-04-24 17:53:51 -0700

    Facial Mapper sanity check on Mac

--- facialMapping.py
+++ facialMapping.py
@@ -3,7 +3,12 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-haar_face_cascade = cv2.CascadeClassifier('./venv/lib/python3.5/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+# Mac Path
+haar_face_cascade = cv2.CascadeClassifier('./venv/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
+# Ubuntu Path is different
+# haar_face_cascade = cv2.CascadeClassifier('./venv/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_default.xml')
+
 
 while(True):
     # Capture frame-by-frame
